513202c0871427c079994457703fd975921cf95a
Timothy Sutzer <timothysutzer@gmail.com>
2020-03-10 15:27:21 -0400

    push 2 more methods

--- 12-Capstone/java/src/main/java/com/techelevator/npgeek/JdbcParkDao.java
+++ 12-Capstone/java/src/main/java/com/techelevator/npgeek/JdbcParkDao.java
@@ -1,18 +1,60 @@
 package com.techelevator.npgeek;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcParkDao implements ParkDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JdbcParkDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	@Override
 	public List<Park> getAllParks() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Park> getAllParks = new ArrayList<>();
+		String sqlSelectAllParks = "SELECT * FROM park";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSelectAllParks);
+		while (results.next()) {
+			getAllParks.add(mapRowToPark(results));
+		}
+		return getAllParks;
+	}
+
+	private Park mapRowToPark(SqlRowSet row) {
+		Park park = new Park();
+		park.setParkCode(row.getString("parkcode"));
+		park.setParkName(row.getString("parkname"));
+		park.setState(row.getString("state"));
+		park.setDescription(row.getString("description"));
+		park.setAcerage(row.getInt("acerage"));
+		park.setElevationInFeet(row.getInt("elevationinfeet"));
+		park.setMilesOfTrail(row.getDouble("milesoftrail"));
+		park.setNumberOfCampsites(row.getInt("numberofcampsites"));
+		park.setClimate(row.getString("climate"));
+		park.setYearFounded(row.getInt("yearfounded"));
+		park.setAnnualVistors(row.getInt("annualvisitorcount"));
+		park.setQuoteAuthor(row.getString("inspirationqoutesource"));
+		park.setQuote(row.getString("inspirationalquote"));
+		park.setDescription(row.getString("parkdescription"));
+		park.setEntryFee(row.getInt("entryfee"));
+		park.setAnimalSpecies(row.getInt("numberofanimalspecies"));
+		return park;
 	}
 
 	@Override
 	public Park getParkByCode(String parkCode) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
--- 12-Capstone/java/src/main/java/com/techelevator/npgeek/JdbcSurveyDao.java
+++ 12-Capstone/java/src/main/java/com/techelevator/npgeek/JdbcSurveyDao.java
@@ -1,17 +1,62 @@
 package com.techelevator.npgeek;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcSurveyDao implements SurveyDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JdbcSurveyDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	@Override
 	public void saveSurvey(Survey survey) {
-		// TODO Auto-generated method stub
+		String sqlInsertSurveyResult = "INSERT INTO survey_result "
+				+ "(surveyid, parkcode, emailaddress, state, activitylevel) " + "VALUES (?, ?, ?, ?, ?)";
+		int newId = getNextId();
+		jdbcTemplate.update(sqlInsertSurveyResult, newId, survey.getParkCode(), survey.getEmail(), survey.getState(),
+				survey.getActivityLevel());
 
 	}
 
-	@Override
-	public int getFavoritePark() {
-		// TODO Auto-generated method stub
-		return 0;
+	private int getNextId() {
+		String sqlSelectNextId = "SELECT NEXTVAL('seq_surveyid')";
+		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlSelectNextId);
+		if (result.next()) {
+			return result.getInt(1);
+		} else {
+			throw new RuntimeException("Something went wrong while getting the next survey id");
+		}
 	}
 
+//	@Override
+//	public List<Park> getTopFiveParks() {
+//		List<park> topFiveParks  = new ArrayList<>();
+//		String sqlCountVotes = "SELECT COUNT(parkcode) AS park_vote, parkcode FROM "
+//		+ "survey_result GROUP BY parkcode ORDER BY park_vote desc limit 5";
+//		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlCountVotes);
+//		if(results.next()) {
+//			park = mapRowToPark(results);
+//		return topFiveParks;
+//	}
+
+//	private Weather mapRowToWeather(SqlRowSet row) {
+//		Weather weather = new Weather();
+//		weather.setForecastValue(row.getInt("fivedayforecastvalue"));
+//		weather.setLowInF(row.getInt("low"));
+//		weather.setHighInF(row.getInt("high"));
+//		weather.setForecast(row.getString("forecast"));
+//		return weather;
+//	}
 }
