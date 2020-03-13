4cbd70738de6e1bd7c77035f4e0a05fa112806dd
Willis Koa <williskoa@yahoo.com>
2020-03-13 09:27:19 -0400

    merge woopsie again

--- 12-Capstone/java/src/main/java/com/techelevator/npgeek/JdbcWeatherDao.java
+++ 12-Capstone/java/src/main/java/com/techelevator/npgeek/JdbcWeatherDao.java
@@ -30,19 +30,6 @@ public class JdbcWeatherDao implements WeatherDao {
 			weatherForecasts.add(mapRowToWeather(results));
 		}
 		if(tempPreference.equals("C")) {
-<<<<<<< HEAD
-			for(int i = 0; i< weatherForecasts.size(); i++) {
-				int tempLowInFahrenheit = weatherForecasts.get(i).getLowTemp();
-				int tempHighInFahrenheit = weatherForecasts.get(i).getHighTemp();
-				Double lowInC = ((tempLowInFahrenheit - 32) / 1.8);
-				Double highInC = ((tempHighInFahrenheit - 32) / 1.8);
-				int tempLowInCelcius = lowInC.intValue();
-				int tempHighInCelcius = highInC.intValue();
-			//	int tempLowInCelcius = (int) ((tempLowInFahrenheit - 32) / 1.8);
-			//	int tempHighInCelcius = (int) ((tempHighInFahrenheit - 32) / 1.8);
-				weatherForecasts.get(i).setLowTemp(tempLowInCelcius);
-				weatherForecasts.get(i).setHighTemp(tempHighInCelcius); 
-=======
 			for(int i = 0; i < weatherForecasts.size(); i++) {
 				double tempLowInFahrenheit = weatherForecasts.get(i).getLowTemp();
 				double tempHighInFahrenheit = weatherForecasts.get(i).getHighTemp();
@@ -52,7 +39,6 @@ public class JdbcWeatherDao implements WeatherDao {
 				int highCInt = (int) Math.round(tempHighInCelcius);
 				weatherForecasts.get(i).setLowTemp(lowCInt);
 				weatherForecasts.get(i).setHighTemp(highCInt);
->>>>>>> d3f719e23ed4cf8207796b7543d1322d4f265e77
 			}
 		}
 		return weatherForecasts; 
